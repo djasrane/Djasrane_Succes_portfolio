04a51599f05b2f661e02673e35d189100a2b0053
Succès Djasrane Tolba <succesdjasranetolba@gmail.com>
2025-08-12 12:22:59 +0100

    deuxieme commites

---
+++ JS/Switcher.js
@@ -0,0 +1,28 @@
+  const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+  styleSwitcherToggle.addEventListener ("click", () => { 
+    document.querySelector(".style-switcher").classList.toggle("open");
+
+  })
+
+  window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+      document.querySelector(".style-switcher").classList.remove("open");
+    }
+  });
+
+  const alternateStyle = document.querySelectorAll (".alternate-style");
+  function setActiveStyle(color)
+  {
+    alternateStyle.forEach(style) => {
+      if(color === stylegetAttribute("title"))
+      {
+        style.removeAttribute("disabled");
+      }
+      else 
+      {
+        style.setAttribute("disabled", "true");
+      }
+    }
+  }
+
